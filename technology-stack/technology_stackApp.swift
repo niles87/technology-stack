c6c7611195dd5a60d368b18a0c00acff594e5ad0
niles87 <nbing19@gmail.com>
2021-01-14 16:29:41 -0800

    Initial Commit

---
+++ technology-stack/technology_stackApp.swift
@@ -0,0 +1,17 @@
+//
+//  technology_stackApp.swift
+//  technology-stack
+//
+//  Created by Niles Bingham on 1/14/21.
+//
+
+import SwiftUI
+
+@main
+struct technology_stackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
