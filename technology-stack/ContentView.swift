bbb510cc9558c1921d5ebf36aaa6db2bfd63f0c3
niles87 <nbing19@gmail.com>
2021-01-15 18:06:41 -0800

    building basic layout

--- technology-stack/ContentView.swift
+++ technology-stack/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    var items = ["First Item", "Second Item", "Third Item", "Fourth Item", "Fifth Item", "Sixth Item"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                TitleBar()
+                ScrollView {
+                    LazyVGrid(columns: columns) {
+                        ForEach(items, id: \.self) {item in
+                            Text(item)
+                                .frame(width: 150, height: 150)
+                                .background(Color(.separator))
+                                .padding()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
