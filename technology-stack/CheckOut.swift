cf436ee689cbbc8083590e4048fd9d76975c738f
niles87 <nbing19@gmail.com>
2021-01-17 15:39:21 -0800

    started the checkout view

---
+++ technology-stack/CheckOut.swift
@@ -0,0 +1,17 @@
+//
+//  CheckOut.swift
+//  technology-stack
+//
+//  Created by Niles Bingham on 1/17/21.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var total: Double
+    var body: some View {
+        VStack {
+            Text("Checkout!")
+        }.navigationTitle("Checkout")
+    }
+}
