55c3deac7e1306b03102271efcf7b8a48b2d72e3
niles87 <nbing19@gmail.com>
2021-01-18 18:03:25 -0800

    Implement search functionality, added search function

--- technology-stack/TitleBar.swift
+++ technology-stack/TitleBar.swift
@@ -10,26 +10,64 @@ import SwiftUI
 struct TitleBar: View {
     @State var search: String = ""
     @State private var isEditing = false
+    @State private var openSearch = false
     @Binding var cart: [Item]
+    @Binding var itemList: [Item]
+    @Binding var searchedItems: [Item]
     var body: some View {
         HStack {
             Text("Tech Stack")
                 .font(.title)
-            TextField(
-                "search",
-                text: $search
-            ) { isEditing in
-                self.isEditing = isEditing
-                
-            }.autocapitalization(.none)
-            .disableAutocorrection(true)
-            .frame(width: 150)
-            .border(Color(UIColor.separator))
-            Button(action: {
-                print("clicking search")
-            }, label: {
-                Image(systemName: "magnifyingglass")
-            })
+            Spacer()
+            if openSearch {
+                TextField(
+                    "search",
+                    text: $search
+                ) { isEditing in
+                    self.isEditing = isEditing
+                    
+                }.autocapitalization(.none)
+                .disableAutocorrection(true)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 150)
+                .border(Color(UIColor.separator))
+                Button(action: {
+                    searchItems()
+                }, label: {
+                    Image(systemName: "magnifyingglass")
+                })
+            } else {
+                Button(action: {
+                    openSearch.toggle()
+                }, label: {
+                    Image(systemName: "magnifyingglass")
+                })
+            }
+            NavigationLink(
+                destination: CartView(cart: $cart),
+                label: {
+                    Image(systemName: "cart.circle")
+                })
+        }
+        .padding(5)
+        .frame(width: UIScreen.main.bounds.width, height: 50)
+        .background(Color(.quaternaryLabel))
+    }
+    
+    func searchItems() {
+        self.searchedItems = search.isEmpty ? itemList : itemList.filter {$0.name.contains(search)}
+        self.search = ""
+        self.openSearch = false 
+    }
+}
+
+struct NavBar: View {
+    @Binding var cart: [Item]
+    var body: some View {
+        HStack{
+            Text("Tech Stack")
+                .font(.title)
+            Spacer()
             NavigationLink(
                 destination: CartView(cart: $cart),
                 label: {
