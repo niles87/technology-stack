c20faee2e82ee5aa201e724dff682b462bd16ce2
niles87 <nbing19@gmail.com>
2021-01-19 17:46:19 -0800

    Add form for check out

--- technology-stack/CheckOut.swift
+++ technology-stack/CheckOut.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    @State private var signIn = false
     var total: Double
     var body: some View {
         VStack {
             Text("Checkout!")
+            Button(action: {
+                self.signIn.toggle()
+            }, label: {
+                Text("Sign in to Checkout")
+            }).sheet(isPresented: $signIn, content: {
+                SignInForm()
+            })
         }.navigationTitle("Checkout")
+    }
+}
+
+struct SignInForm: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        VStack {
+            TextField("Email", text: $email)
+            SecureField("Password", text: $password)
+            Button(action: {}, label: {
+                Text("Submit")
+            })
+        }
     }
 }
