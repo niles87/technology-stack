e54d8fcc4ed36c036cf5c6d5e2929847340af131
niles87 <nbing19@gmail.com>
2021-01-22 11:13:36 -0800

    Get user function

--- technology-stack/AuthHelper.swift
+++ technology-stack/AuthHelper.swift
@@ -11,7 +11,12 @@ struct User {
     var firstName: String
     var lastName: String
     var email: String
-    var password: String?
+    var password: String
+}
+
+struct ExistingUser {
+    var email: String
+    var name: String
 }
 
 class Auth {
@@ -28,7 +33,7 @@ class Auth {
         let parameters: [String: Any] = [
             "name": "\(user.firstName) \(user.lastName)",
             "email": user.email,
-            "password": user.password!
+            "password": user.password
         ]
         guard let jsonData = try?
                 JSONSerialization.data(withJSONObject: parameters, options: []) else {
@@ -41,10 +46,10 @@ class Auth {
         
         let semaphore = DispatchSemaphore(value: 0)
         URLSession.shared.dataTask(with: request) {(data, response, error) in
+            semaphore.signal()
             if let error = error {
                 print("Error posting new user: \(error)")
             }
-            semaphore.signal()
             if let response = response {
                 let res = response as! HTTPURLResponse
                 
@@ -55,5 +60,44 @@ class Auth {
         }.resume()
         semaphore.wait()
         return success
+    }
+    
+    func getUser(email: String, password: String) -> ExistingUser {
+        var exUser = ExistingUser(email: "", name: "")
+        let url = URL(string: "")
+        var request = URLRequest(url: url!)
+        
+        let parameters: [String:Any] = [
+            "email": email,
+            "password": password
+        ]
+        
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
+            return exUser
+        }
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            semaphore.signal()
+            if let error = error {
+                print("error \(error)")
+            }
+            if let response = response {
+                let res = response as! HTTPURLResponse
+                
+                if res.statusCode == 200 {
+                    print(response)
+                    exUser.name = ""
+                    exUser.email = ""
+                }
+            }
+        }.resume()
+        
+        semaphore.wait()
+        return exUser
     }
 }
